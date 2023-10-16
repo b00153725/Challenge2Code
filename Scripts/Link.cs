19d2644fbc2b0a57e2695952427655e51463090e
b00153725 <120036628+b00153725@users.noreply.github.com>
2023-10-16 21:27:12 +0100

    Add files via upload

---
+++ Scripts/Link.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+using UnityEngine.UI;
+
+public class Link : MonoBehaviour 
+{
+
+	public void OpenLinkJSPlugin()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://github.com/b00153725/Challenge-2.git");
+		#endif
+	}
+
+	[DllImport("__Internal")]
+	private static extern void openWindow(string url);
+
+}
---
+++ Scripts/OpenLInk.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLInk : MonoBehaviour
+{
+    public void OpenChannel()
+    {
+        Application.OpenURL("https://github.com/b00153725/Challenge-2.git");
+    }
+}
